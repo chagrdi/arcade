b357b08e1ffe03c8abd79a479f5a14d2095c29ad
“chagrdigit config --global user.name “Charlotte Grandjean <charlotte.grandjean@yahoo.fr>
2019-03-06 13:56:05 +0100

    backgroundok

--- arcade-css/src/App.js
+++ arcade-css/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         <Classement></Classement>
         <Footer></Footer>
         
+        
       </div>
     );
   }
--- arcade-css/src/components/Header.js
+++ arcade-css/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 class Header extends React.Component{
     render(){
         return(
-            <div className="Coin">Test</div>
+            <div className="Coin">JEU ARCADE</div>
     
         
         );
